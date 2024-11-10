843563972c8d33830c99e7f9ac70819402f429fe
shaddollxz <shaddollxz@163.com>
2024-11-10 12:45:35 +0800

    feat: remove some link track params

--- src/commands/star.ts
+++ src/commands/star.ts
@@ -8,7 +8,7 @@ import type { CustomError, DeepPartial } from "@/types";
 
 export async function starCommand(
   msg: string,
-  defaultProperties?: DeepPartial<BookMarkClientProps>
+  defaultProperties?: DeepPartial<BookMarkClientProps>,
 ) {
   if (!msg) throw { postUser: true, msg: "没有输入内容" } as CustomError;
 
@@ -31,13 +31,49 @@ export async function starCommand(
 }
 
 const ADDRESS_MAP = [
-  { host: ["bilibili.com", "b23.tv"], name: "bilibili" },
+  {
+    host: ["bilibili.com", "b23.tv"],
+    name: "bilibili",
+    format: (str: string) => {
+      const url = new URL(str);
+
+      if (url.pathname.includes("/video/BV")) {
+        return replaceSearchParams(url, null).toString();
+      }
+
+      return str;
+    },
+  },
   { host: ["xiaoheihe.cn"], name: "小黑盒" },
   { host: ["skyland.com"], name: "森空岛" },
   { host: ["github.com"], name: "github" },
   { host: ["pan.baidu.com"], name: "网盘" },
-  { host: ["tieba.baidu.com"], name: "贴吧" },
+  {
+    host: ["tieba.baidu.com"],
+    name: "贴吧",
+    format: (str: string) => {
+      const url = new URL(str);
+
+      return replaceSearchParams(url, null).toString();
+    },
+  },
   { host: ["zhihu.com"], name: "知乎" },
+  {
+    host: ["weixin.qq.com"],
+    name: "微信",
+    format: (str: string) => {
+      const url = new URL(str);
+
+      const newSearchParams = new URLSearchParams({
+        __biz: url.searchParams.get("__biz")!,
+        mid: url.searchParams.get("mid")!,
+        idx: url.searchParams.get("idx")!,
+        sn: url.searchParams.get("sn")!,
+      });
+
+      return replaceSearchParams(url, newSearchParams).toString();
+    },
+  },
 ];
 
 function analyserShareContent(contentStr: string) {
@@ -82,6 +118,8 @@ function analyserShareContent(contentStr: string) {
       if (acc) return acc;
 
       if (cur.host.some((url) => row.properties.link!.includes(url))) {
+        cur.format && (row.properties.link = cur.format(row.properties.link!));
+
         return cur.name;
       }
 
@@ -90,4 +128,20 @@ function analyserShareContent(contentStr: string) {
   }
 
   return row;
+}
+
+function replaceSearchParams(url: URL, searchParams: URLSearchParams | null) {
+  const newURL = new URL(url);
+
+  Array.from(newURL.searchParams.keys()).forEach((key) =>
+    newURL.searchParams.delete(key),
+  );
+
+  if (searchParams) {
+    Array.from(searchParams.entries()).forEach((entry) =>
+      newURL.searchParams.set(...entry),
+    );
+  }
+
+  return newURL;
 }
